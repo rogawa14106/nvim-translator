959962d3cc5b8cbf3140bd71787726e91d788b48
rogawa14106 <ookawa14106@gmail.com>
2024-10-18 12:11:37 +0900

    chg: setupで初期化するよう修正

--- lua/nvim-translator.lua
+++ lua/nvim-translator.lua
@@ -1,15 +1,20 @@
 local M = {}
 
+-- import modules
 local nt_config = require('nvim-translator.config')
+local translator = require('nvim-translator.translator')
+
+-- initialize plugin
+local function init()
+    print('hello nvim-translator')
+    local translate = translator.translate
+end
 
 ---@type fun(user_config: NTConfig): nil
 function M.setup(user_config)
     -- override default nvim-translator configuration
     local config = nt_config.build_config(user_config)
-end
-
-function M.init()
-    print('hello nvim-translator')
+    init()
 end
 
 return M
