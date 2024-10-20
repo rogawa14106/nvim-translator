3cf2771e0924cde2ffbf3d0c01fe9e5dbe5e2c4d
rogawa14106 <ookawa14106@gmail.com>
2024-10-20 13:03:32 +0900

    fix: 0.9.0で動作確認ができたので、サポート対象のバージョンを変更。コンフィグの適用方法を修正。

--- plugin/nvim-translator.lua
+++ plugin/nvim-translator.lua
@@ -3,7 +3,7 @@ if vim.g.loaded_nvim_translator then
     return
 end
 vim.g.loaded_nvim_translator = true
-
+--テスト
 -- nvim check
 if vim.fn.has('nvim') ~= 1 then
     vim.notify("this plugin only support nvim.", vim.log.levels.ERROR)
@@ -14,10 +14,10 @@ end
 ---@type (vim.Version)?
 local nvim_local_v = vim.version.parse(vim.fn.system({ 'nvim', '-v' }), { strict = false })
 ---@type number[]
-local nvim_support_v = { 0, 10, 0 }
+local nvim_support_v = { 0, 9, 0 }
 if (nvim_local_v ~= nil) and (vim.version.lt(nvim_local_v, nvim_support_v)) then
     vim.notify(
-        "this plugin only support nvim version >="
+        "[nvim-translator] this plugin only support nvim version >="
         .. nvim_support_v[1] .. "." .. nvim_support_v[2] .. "." .. nvim_support_v[3]
         .. "\nI won't know it works unless you try it",
         vim.log.levels.WARN
@@ -28,19 +28,4 @@ end
 vim.cmd("set runtimepath+=~/work/01_dev/nvim_plugin/nvim-translator")
 
 ---initialize nvim-translator on default config
----@type NTConfig
-local default_config = {
-    keymap = {
-        {
-            src = "en",
-            dst = "ja",
-            key = "<Leader>?",
-        },
-        {
-            src = "ja",
-            dst = "en",
-            key = "<Leader>g?",
-        }
-    },
-}
-require("nvim-translator").setup(default_config)
+require("nvim-translator").setup()
