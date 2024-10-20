0a809690e3ee9ee19cb30177e4d044ea2b538479
rogawa14106 <ookawa14106@gmail.com>
2024-10-20 14:12:49 +0900

    fix: 言語の型を厳格に設定。URLエンコードを修正

--- lua/nvim-translator/translator.lua
+++ lua/nvim-translator/translator.lua
@@ -22,6 +22,142 @@ M.REQUEST_PARAM_KEY = {
 ---maximum translatable text length 
 ---@type integer
 M.TEXT_LEN_LIMIT = 3000
+
+---@alias LANG @string literal to specify translate language
+---| "af" Afrikaans
+---| "sq" Albanian
+---| "am" Amharic
+---| "ar" Arabic
+---| "hy" Armenian
+---| "as" Assamese
+---| "ay" Aymara
+---| "az" Azerbaijani
+---| "bm" Bambara
+---| "eu" Basque
+---| "be" Belarusian
+---| "bn" Bengali
+---| "bho" Bhojpuri
+---| "bs" Bosnian
+---| "bg" Bulgarian
+---| "ca" Catalan
+---| "ceb" Cebuano
+---| "zh-CN" Chinese (Simplified) (BCP-47)
+---| "zh-TW" Chinese (Traditional) (BCP-47)
+---| "co" Corsican
+---| "hr" Croatian
+---| "cs" Czech
+---| "da" Danish
+---| "dv" Dhivehi
+---| "doi" Dogri
+---| "nl" Dutch
+---| "en" English
+---| "eo" Esperanto
+---| "et" Estonian
+---| "ee" Ewe
+---| "fil" Filipino (Tagalog)
+---| "fi" Finnish
+---| "fr" French
+---| "fy" Frisian
+---| "gl" Galician
+---| "ka" Georgian
+---| "de" German
+---| "el" Greek
+---| "gn" Guarani
+---| "gu" Gujarati
+---| "ht" Haitian Creole
+---| "ha" Hausa
+---| "haw" Hawaiian
+---| "he" Hebrew
+---| "hi" Hindi
+---| "hmn" Hmong
+---| "hu" Hungarian
+---| "is" Icelandic
+---| "ig" Igbo
+---| "ilo" Ilocano
+---| "id" Indonesian
+---| "ga" Irish
+---| "it" Italian
+---| "ja" Japanese
+---| "jv" Javanese
+---| "kn" Kannada
+---| "kk" Kazakh
+---| "km" Khmer
+---| "rw" Kinyarwanda
+---| "gom" Konkani
+---| "ko" Korean
+---| "kri" Krio
+---| "ku" Kurdish
+---| "ckb" Kurdish (Sorani)
+---| "ky" Kyrgyz
+---| "lo" Lao
+---| "la" Latin
+---| "lv" Latvian
+---| "ln" Lingala
+---| "lt" Lithuanian
+---| "lg" Luganda
+---| "lb" Luxembourgish
+---| "mk" Macedonian
+---| "mai" Maithili
+---| "mg" Malagasy
+---| "ms" Malay
+---| "ml" Malayalam
+---| "mt" Maltese
+---| "mi" Maori
+---| "mr" Marathi
+---| "mni-Mtei" Meiteilon (Manipuri)
+---| "lus" Mizo
+---| "mn" Mongolian
+---| "my" Myanmar (Burmese)
+---| "ne" Nepali
+---| "no" Norwegian
+---| "ny" Nyanja (Chichewa)
+---| "or" Odia (Oriya)
+---| "om" Oromo
+---| "ps" Pashto
+---| "fa" Persian
+---| "pl" Polish
+---| "pt" Portuguese (Portugal, Brazil)
+---| "pa" Punjabi
+---| "qu" Quechua
+---| "ro" Romanian
+---| "ru" Russian
+---| "sm" Samoan
+---| "sa" Sanskrit
+---| "gd" Scots Gaelic
+---| "nso" Sepedi
+---| "sr" Serbian
+---| "st" Sesotho
+---| "sn" Shona
+---| "sd" Sindhi
+---| "si" Sinhala (Sinhalese)
+---| "sk" Slovak
+---| "sl" Slovenian
+---| "so" Somali
+---| "es" Spanish
+---| "su" Sundanese
+---| "sw" Swahili
+---| "sv" Swedish
+---| "tl" Tagalog (Filipino)
+---| "tg" Tajik
+---| "ta" Tamil
+---| "tt" Tatar
+---| "te" Telugu
+---| "th" Thai
+---| "ti" Tigrinya
+---| "ts" Tsonga
+---| "tr" Turkish
+---| "tk" Turkmen
+---| "ak" Twi (Akan)
+---| "uk" Ukrainian
+---| "ur" Urdu
+---| "ug" Uyghur
+---| "uz" Uzbek
+---| "vi" Vietnamese
+---| "cy" Welsh
+---| "xh" Xhosa
+---| "yi" Yiddish
+---| "yo" Yoruba
+---| "zu" Zulu
 -- }}}
 
 -- text loader {{{
@@ -58,6 +194,22 @@ M.load_text = function(type)
 end
 -- }}}
 
+-- url encoding
+local char_to_hex = function(c)
+  return string.format("%%%02X", string.byte(c))
+end
+
+local function url_encode(url)
+  if url == nil then
+    return
+  end
+  url = url:gsub("\n", "\r\n")
+  url = url:gsub("([^%w ])", char_to_hex)
+  url = url:gsub(" ", "+")
+  return url
+end
+
+
 --- Create reqest paramater to hit the translate API{{{
 ---@type fun(text: string, src: LANG, dst: LANG): string?
 M.create_req_params = function(text, src, dst)
@@ -68,37 +220,37 @@ M.create_req_params = function(text, src, dst)
     end
 
     -- format text used in request parameter
-    local req_text = text
+    local req_text = url_encode(text)
     -- substitute characters that can't use in url
-    local forbidden_chars = {
-        "%s+", "!", '"', "#", "%$",
-        -- "%",
-        "&", "'", "%(", "%)", "*",
-        "+", ",", "/", ":", ";",
-        "<", "=", ">", "?", "@",
-        "%[", "%]",
-        -- "^",
-        "`", "%{", "%|", "%}", "~",
-    }
-    local url_encodes = {
-        "%%20", "%%21", "%%22", "%%23", "%%24",
-        -- "%%25",
-        "%%26", "%%27", "%%28", "%%29", "%%2A",
-        "%%2B", "%%2C", "%%2F", "%%3A", "%%3B",
-        "%%3C", "%%3D", "%%3E", "%%3F", "%%40",
-        "%%5B", "%%5D",
-        -- "%%5E",
-        "%%60", "%%7B", "%%7C", "%%7D", "%%7E",
-    }
-    for i = 1, #forbidden_chars do
-        req_text = string.gsub(req_text, forbidden_chars[i], url_encodes[i])
-    end
+    -- local forbidden_chars = {
+        -- "%s+", "!", '"', "#", "%$",
+        -- -- "%",
+        -- "&", "'", "%(", "%)", "*",
+        -- "+", ",", "/", ":", ";",
+        -- "<", "=", ">", "?", "@",
+        -- "%[", "%]",
+        -- -- "^",
+        -- "`", "%{", "%|", "%}", "~",
+    -- }
+    -- local url_encodes = {
+        -- "%%20", "%%21", "%%22", "%%23", "%%24",
+        -- -- "%%25",
+        -- "%%26", "%%27", "%%28", "%%29", "%%2A",
+        -- "%%2B", "%%2C", "%%2F", "%%3A", "%%3B",
+        -- "%%3C", "%%3D", "%%3E", "%%3F", "%%40",
+        -- "%%5B", "%%5D",
+        -- -- "%%5E",
+        -- "%%60", "%%7B", "%%7C", "%%7D", "%%7E",
+    -- }
+    -- for i = 1, #forbidden_chars do
+        -- req_text = string.gsub(req_text, forbidden_chars[i], url_encodes[i])
+    -- end
+
     -- create reqest paramater
     local req_params = ""
     req_params = req_params .. "?" .. M.REQUEST_PARAM_KEY.txt .. "=" .. req_text
     req_params = req_params .. "&" .. M.REQUEST_PARAM_KEY.src .. "=" .. src
     req_params = req_params .. "&" .. M.REQUEST_PARAM_KEY.dst .. "=" .. dst
-
     return req_params
 end
 -- }}}
@@ -118,5 +270,14 @@ M.translate = function(text, src, dst, on_success, on_err)
     }
     async.execute_cmd_async(cmd, cmd_args, on_success, on_err)
 end
+
+--debug
+local function debug_fun()
+    local text = "こんにちわ 私はrogawaです"
+    local on_success = function(data) print(data) end
+    local on_err = function(data) print(data) end
+    M.translate(text, "ja", "en", on_success, on_err)
+end
+debug_fun()
 
 return M
