46cb4db313f26fd2845c06369fc467ca0e45813f
rogawa14106 <ookawa14106@gmail.com>
2024-10-20 21:00:00 +0900

    add: wgetに対応

--- lua/nvim-translator/translator.lua
+++ lua/nvim-translator/translator.lua
@@ -1,28 +1,29 @@
 local M = {}
 local async = require('lib.async')
 
---- Define specification of translate API{{{
---- The URL of translate api.
+-- Define specification of translate API{{{
+--- The URL of translate api.{{{
 ---@type string
-M.URL_TRANSLATOR =
+URL_TRANSLATOR =
 "https://script.google.com/macros/s/AKfycbz3W9G4Jm4GFF_R74CsgsOOQJvDCw2kXIUecRQEx9uHJ56xjH9gLyoVDcsYJ198hjE/exec"
 --v1.0 "https://script.google.com/macros/s/AKfycbwazxusB41dZgqxLMuQ1mn6177dGGISodFDv4-yaeKuTr45BaDXqOAupIiceJyBCEs/exec"
-
---- Request paramaters of translate api.
+-- }}}
+--- Request paramaters of translate api.{{{
 ---@class RequestParamKey
 ---@field src string
 ---@field dst string
 ---@field txt string
-M.REQUEST_PARAM_KEY = {
+REQUEST_PARAM_KEY = {
     src = "source",
     dst = "target",
     txt = "text"
 }
-
----maximum translatable text length 
+-- }}}
+--- maximum translatable text length {{{
 ---@type integer
-M.TEXT_LEN_LIMIT = 3000
-
+TEXT_LEN_LIMIT = 3000
+-- }}}
+--- Language types{{{
 ---@alias LANG @string literal to specify translate language
 ---| "af" Afrikaans
 ---| "sq" Albanian
@@ -159,6 +160,7 @@ M.TEXT_LEN_LIMIT = 3000
 ---| "yo" Yoruba
 ---| "zu" Zulu
 -- }}}
+--}}}
 
 -- text loader {{{
 -- load text selected on visual mode
@@ -194,27 +196,27 @@ M.load_text = function(type)
 end
 -- }}}
 
--- url encoding
+-- url encoding{{{
 local char_to_hex = function(c)
-  return string.format("%%%02X", string.byte(c))
+    return string.format("%%%02X", string.byte(c))
 end
 
 local function url_encode(url)
-  if url == nil then
-    return
-  end
-  url = url:gsub("\n", "\r\n")
-  url = url:gsub("([^%w ])", char_to_hex)
-  url = url:gsub(" ", "+")
-  return url
+    if url == nil then
+        return
+    end
+    url = url:gsub("\n", "\r\n")
+    url = url:gsub("([^%w ])", char_to_hex)
+    url = url:gsub(" ", "+")
+    return url
 end
-
+-- }}}
 
 --- Create reqest paramater to hit the translate API{{{
 ---@type fun(text: string, src: LANG, dst: LANG): string?
 M.create_req_params = function(text, src, dst)
     -- source text must be less than 3000 characters
-    if string.len(text) > M.TEXT_LEN_LIMIT then
+    if string.len(text) > TEXT_LEN_LIMIT then
         vim.notify("the text must be less than 3,000 characters", vim.log.levels.WARN)
         return nil
     end
@@ -227,27 +229,33 @@ M.create_req_params = function(text, src, dst)
 
     -- create reqest paramater
     local req_params = ""
-    req_params = req_params .. "?" .. M.REQUEST_PARAM_KEY.txt .. "=" .. req_text
-    req_params = req_params .. "&" .. M.REQUEST_PARAM_KEY.src .. "=" .. src
-    req_params = req_params .. "&" .. M.REQUEST_PARAM_KEY.dst .. "=" .. dst
+    req_params = req_params .. "?" .. REQUEST_PARAM_KEY.txt .. "=" .. req_text
+    req_params = req_params .. "&" .. REQUEST_PARAM_KEY.src .. "=" .. src
+    req_params = req_params .. "&" .. REQUEST_PARAM_KEY.dst .. "=" .. dst
     return req_params
 end
 -- }}}
 
+--- exec translation{{{
 ---@param text string
 ---@param src LANG
 ---@param dst LANG
 ---@param on_success function
 ---@param on_err function
 M.translate = function(text, src, dst, on_success, on_err)
-    local cmd = "curl"
-    local url = M.URL_TRANSLATOR .. M.create_req_params(text, src, dst)
-    local cmd_args = {
-        "-sS",
-        "-L",
-        url
+    local url = URL_TRANSLATOR .. M.create_req_params(text, src, dst)
+    local cmd_list = {
+        ['curl'] = { "-sSL", url },
+        ['wget'] = { "-qO-", url }
     }
-    async.execute_cmd_async(cmd, cmd_args, on_success, on_err)
+    for cmd, cmd_args in pairs(cmd_list) do
+        if vim.fn.executable(cmd) == 1 then
+            async.execute_cmd_async(cmd, cmd_args, on_success, on_err)
+            return
+        end
+    end
+    vim.notify('curl or wget is required to perform the translation.', vim.log.levels.ERROR)
 end
+-- }}}
 
 return M
